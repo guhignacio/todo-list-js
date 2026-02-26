fc733789adf7769b3be3e749880e2d3fe698f3d4
Guhignacio <lgcosta2004@gmail.com>
2026-02-26 16:33:58 -0300

    Adicionado a função de excluir tarefa

--- script.js
+++ script.js
@@ -3,13 +3,39 @@ const btnAdicionar = document.getElementById('btnAdicionar');
 const lista = document.querySelector('ul');
 
 btnAdicionar.addEventListener('click', () => {
+    console.log('Você adicionou: ' + tarefa.value);
+
     const novaTarefa = tarefa.value;
 
-    if(novaTarefa === '') return;
-
+    console.log(
+        'Texto:', novaTarefa,
+        'Tamanho:', novaTarefa.length,
+        'Tipo:', typeof novaTarefa
+    )
+    if (novaTarefa === '') {
+        console.log('Tarefa vazia');
+        alert('A tarefa não pode ser vazia');
+        return;
+    } else if (novaTarefa.length < 3) {
+        alert('A tarefa deve ter mais de 3 caracteres');
+        console.log('Tarefa muito curta');
+        return;
+    }
+    
+    console.log('Validações ok');
+    
     const item = document.createElement('li');
     item.textContent = novaTarefa;
 
+    const btnExcluir= document.createElement('button');
+    btnExcluir.textContent = 'Excluir';
+
+    btnExcluir.addEventListener('click',() => {
+        item.remove();
+    })
+
+    item.appendChild(btnExcluir);
     lista.appendChild(item);
+    
     tarefa.value = '';
 })

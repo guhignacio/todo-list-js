dfb61b6f4e441050e5086809c98e06765c70ae17
Guhignacio <lgcosta2004@gmail.com>
2026-03-01 16:10:52 -0300

    Implementa localStorage

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const tarefa = document.getElementById('nTarefa');
 const btnAdicionar = document.getElementById('btnAdicionar');
 const lista = document.querySelector('ul');
 
+carregarTarefas();
+
 btnAdicionar.addEventListener('click', () => {
     console.log('Você adicionou: ' + tarefa.value);
 
@@ -32,18 +34,57 @@ btnAdicionar.addEventListener('click', () => {
 
     item.appendChild(btnExcluir);
     lista.appendChild(item);
-
+    salvarTarefas();
     tarefa.value = '';
 })
 lista.addEventListener('click', (event) => {
     if (event.target.tagName === 'BUTTON') {
         event.target.parentElement.remove();
+        salvarTarefas();
         return;
     }
 
-    if (event.target.classList.contains('concluida')) {
-        event.target.classList.remove('concluida');
-    } else {
-        event.target.classList.add('concluida');
-    }
+    if (event.target.tagName === 'LI') {
+        event.target.classList.toggle('concluida');
+        salvarTarefas();
+    } 
 });
+
+function salvarTarefas() {
+    const itens = lista.querySelectorAll('li');
+    const tarefas = [];
+
+    itens.forEach(item => {
+        tarefas.push({
+            texto: item.firstChild.textContent,
+            concluida: item.classList.contains('concluida')
+        });
+    });
+
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+
+}
+
+function carregarTarefas() {
+    const tarefasSalvas = localStorage.getItem('tarefas');
+
+    if (!tarefasSalvas) return;
+
+    const tarefas = JSON.parse(tarefasSalvas);
+
+    tarefas.forEach(tarefa => {
+        const item = document.createElement('li');
+        item.textContent = tarefa.texto;
+
+        if (tarefa.concluida) {
+            item.classList.add('concluida');
+        }
+
+        const btnExcluir = document.createElement('button');
+        btnExcluir.textContent = 'Excluir';
+
+        item.appendChild(btnExcluir);
+        lista.appendChild(item);
+
+    });
+}
